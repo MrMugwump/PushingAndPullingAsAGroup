0f94b7834bf063483721fc19741977bfde3b03e3
Spencer <sbarton9258@gmail.com>
2019-01-25 10:50:29 -0800

    My name thing

--- PushingAndPullingAsAGroup/src/PushAndPull.java
+++ PushingAndPullingAsAGroup/src/PushAndPull.java
@@ -8,4 +8,8 @@ public class PushAndPull {
     private void printBen(){
         System.out.println("My name is Ben Bridenbaugh");
     }
+
+    private void printSpencer(){
+        System.out.println("I'm Spencer Barton");
+    }
 }
