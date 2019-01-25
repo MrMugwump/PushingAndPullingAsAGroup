96b4460aa709846103f333a7115ba20c813c59f0
alessandro ronchi <aronchi1257@apps4pps.net>
2019-01-25 10:51:25 -0800

    alessandro

--- PushingAndPullingAsAGroup/src/PushAndPull.java
+++ PushingAndPullingAsAGroup/src/PushAndPull.java
@@ -4,8 +4,12 @@ public class PushAndPull {
     }
     private PushAndPull(){
         printBen();
+        printAlessandro();
     }
     private void printBen(){
         System.out.println("My name is Ben Bridenbaugh");
+    }
+    private void printAlessandro{
+        System.out.println("My name is Alessandro");
     }
 }
