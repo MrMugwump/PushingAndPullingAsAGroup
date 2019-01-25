9bbd412b051c293595f88bf21373032796b64287
Ben <benbridenbaugh@gmail.com>
2019-01-25 11:14:43 -0800

    hello

--- PushingAndPullingAsAGroup/src/PushAndPull.java
+++ PushingAndPullingAsAGroup/src/PushAndPull.java
@@ -4,11 +4,11 @@ public class PushAndPull {
     }
     private PushAndPull(){
         printBen();
+        printSpencer();
     }
     private void printBen(){
         System.out.println("My name is Ben Bridenbaugh");
     }
-
     private void printSpencer(){
         System.out.println("I'm Spencer Barton");
     }
